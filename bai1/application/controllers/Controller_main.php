97386c104f53f18c11d8bb02bcf9a3d03741aeca
Nhat Luan <nhatluandao1404@gmail.com>
2019-07-17 09:28:05 +0700

    upload

---
+++ bai1/application/controllers/Controller_main.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Controller_main extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		echo 'hello my world';
+	}
+
+}
+
+/* End of file Controller_main.php */
+/* Location: ./application/controllers/Controller_main.php */
